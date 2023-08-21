8136d8fa8e9edd9caeb9ec131e4fab26e9410584
ManuelGonzalezL <gonzalezm3@ufl.edu>
2023-08-21 14:01:35 -0400

    Landing template added
    Landing template was created, with information just for demonstration. The chart and calendar tools were added to this page. Full Calendar and Chart.js.

--- script.js
+++ script.js
@@ -98,3 +98,55 @@ document.addEventListener('DOMContentLoaded', function() {
         window.location.href = 'login.html'; // Redirect to login.html
     });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  var calendarEl = document.getElementById("calendar");
+  var calendar = new FullCalendar.Calendar(calendarEl, {
+    initialView: "dayGridWeek", // Display the week view
+    weekends: false, // Hide weekends
+    events: [
+      // Define your events here
+    ],
+  });
+  calendar.render();
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Mock data for the graph
+  var data = {
+    labels: [
+      "Math",
+      "Science",
+      "History",
+      "English",
+      "Art",
+      "Music",
+      "Physical Education",
+    ],
+    datasets: [
+      {
+        label: "Number of Students",
+        data: [20, 30, 15, 25, 10, 8, 12],
+        backgroundColor: "rgba(54, 162, 235, 0.6)", // Blue color
+        borderColor: "rgba(54, 162, 235, 1)",
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  // Get the canvas element
+  var ctx = document.getElementById("myChart").getContext("2d");
+
+  // Create the bar chart
+  var myChart = new Chart(ctx, {
+    type: "bar",
+    data: data,
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+    },
+  });
+});
