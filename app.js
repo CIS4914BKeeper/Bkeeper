6d11b4efda5542317e115349a1c2c60354da21aa
Jongho Park <jonghopark@nein-4.local>
2023-11-17 13:58:23 +0100

    Organized codes

--- app.js
+++ app.js
@@ -1,52 +1,51 @@
-const path = require('path');
-const express = require('express');
-const morgan = require('morgan');
-const rateLimit = require('express-rate-limit');
-const helmet = require('helmet');
-const mongoSanitize = require('express-mongo-sanitize');
-const xss = require('xss-clean');
+const path = require("path");
+const express = require("express");
+const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
 // const hpp = require('hpp');
-const cookieParser = require('cookie-parser')
-const cors = require('cors');
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
+const AppError = require("./utils/appError");
+const globalErrorHandler = require("./controllers/errorController");
 
-const AppError = require('./utils/appError');
-const globalErrorHandler = require('./controllers/errorController');
-
-const userRouter = require('./routes/userRoutes');
-const viewRouter = require('./routes/viewRoutes');
+const userRouter = require("./routes/userRoutes");
+const viewRouter = require("./routes/viewRoutes");
 
 const app = express();
 
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: "http://localhost:3000",
     credentials: true,
   }),
 );
 
-app.set('view engine', 'pug');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(
   helmet({ contentSecurityPolicy: false, crossOriginEmbedderPolicy: false }),
 );
 
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 
 const limiter = rateLimit({
   max: 30,
   windowMs: 60 * 60 * 1000,
-  message: 'Too many requests from this IP. Please try again in an hour!',
+  message: "Too many requests from this IP. Please try again in an hour!",
 });
 
-app.use('/api', limiter);
+app.use("/api", limiter);
 
-app.use(express.json({ limit: '10kb' }));
+app.use(express.json({ limit: "10kb" }));
 app.use(cookieParser());
 
 app.use(mongoSanitize());
@@ -57,11 +56,10 @@ app.use(xss());
 //   hpp(),
 // );
 
+app.use("/", viewRouter);
+app.use("/api/v1/users", userRouter);
 
-app.use('/', viewRouter);
-app.use('/api/v1/users', userRouter);
-
-app.all('*', (req, res, next) => {
+app.all("*", (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
